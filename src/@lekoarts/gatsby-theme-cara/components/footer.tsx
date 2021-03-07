ceb2886747443592d78eae59518c780c80981575
jaknowles97 <jaknowles1997@gmail.com>
2021-03-06 20:28:50 -0500

    first commit

---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,45 @@
+
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Link aria-label="Link to the theme author's website" href="https://github.com/jaknowles97">
+          John Knowles
+        </Link>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
